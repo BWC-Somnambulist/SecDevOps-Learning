f447149929d3a5e76087733cad2baf7188dab96a
M Felton <matthew.felton@gmail.com>
2023-01-26 18:44:31 +0000

    First Git Commit after some learning sessions

---
+++ Exceptions.py
@@ -0,0 +1,10 @@
+
+try: 
+    name = Matt
+    print("My name is: " + naem)
+
+except: 
+    print("Something went wrong!")
+
+print("All done!")
+    
---
+++ functions.py
@@ -0,0 +1,12 @@
+def input_number():
+    return int(input("Enter a number: "))
+
+input1 = input_number()
+input2 = input_number()
+input3 = input_number()
+input4 = input_number()
+input5 = input_number()
+input6 = input_number()
+input7 = input_number()
+
+print (input1)
